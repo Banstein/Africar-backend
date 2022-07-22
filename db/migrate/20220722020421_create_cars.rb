63cf5aca8df65ce5b399bcca4cc7130731dbfcef
Islam Bahnas <islambahnas1@gmail.com>
2022-07-22 04:15:07 +0200

    Add F.Keys for cars and reservations

--- db/migrate/20220722020421_create_cars.rb
+++ db/migrate/20220722020421_create_cars.rb
@@ -5,6 +5,8 @@ class CreateCars < ActiveRecord::Migration[7.0]
       t.text :description
       t.string :picture
       t.decimal :price
+      t.references :user, null: false, foreign_key: { to_table: :users }, index:true
+      t.references :car, null: false, foreign_key: { to_table: :cars }, index:true
       t.timestamps
     end
   end
