1151aa340af08a152f4bede9f512d19efe9bc19f
Islam Bahnas <islambahnas1@gmail.com>
2022-07-22 18:54:17 +0200

    Add routes for Cars and Reservations

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   resource :users, only: [:create]
   post "/login", to: "users#login"
-  # root "articles#index"
+  
+  resources :cars, only: %i[index show new create destroy update]
+  resources :Reservations, only: %i[index new create destroy update]
+  root "cars#index"
 end
