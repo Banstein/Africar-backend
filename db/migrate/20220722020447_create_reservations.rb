4c7f885c7e937c0c6ea9867ca873d055184dcf20
Islam Bahnas <islambahnas1@gmail.com>
2022-07-22 18:47:49 +0200

    Fix F.Keys bug
    Sperate F.Keys into standalone migrations to fix "usser relations no exist error".

--- db/migrate/20220722020447_create_reservations.rb
+++ db/migrate/20220722020447_create_reservations.rb
@@ -3,7 +3,7 @@ class CreateReservations < ActiveRecord::Migration[7.0]
     create_table :reservations do |t|
       t.string :city
       t.date :date
-      t.references :user, null: false, foreign_key: { to_table: :users }, index:true
+      
       t.timestamps
     end
   end
