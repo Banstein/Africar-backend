44f704b526954da837dbfc635130e8617202865f
Pascal <pascal@Pascals-MacBook-Pro.local>
2022-07-25 13:30:34 +0200

    Add authorize user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,24 +3,24 @@ class ApplicationController < ActionController::API
     JWT.encode(payload, 'secret')
   end
 
-    def decode_token
+  def decode_token
     # Authorization: Bearer
 
     auth_header = request.headers['Authorization']
-        if auth_header
-            token = auth_header.split[1]
-        begin
-            JWT.decode(token, 'secret', true, algorithm, 'HS256')
-        rescue JWT::DecodeError
-            nil
-        end
+    if auth_header
+      token = auth_header.split[1]
+      begin
+        JWT.decode(token, 'secret', true, algorithm, 'HS256')
+      rescue JWT::DecodeError
+        nil
+      end
     end
   end
 
   def authorized_user
-    decode_token = decode_token()
-    if decode_token
-      user_id = decode_token[0]['user_id']
+    decoded_token = decode_token
+    if decoded_token
+      user_id = decoded_token[0]['user_id']
       @user = User.find_by(id: user_id)
     end
   end
