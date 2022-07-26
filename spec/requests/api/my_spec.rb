a0512b689fd69a7f028bc41b8ef700f663621d58
Nicholas Mutale <nicomutale@gmail.com>
2022-07-26 15:24:57 +0200

    Describe and test car api

--- spec/requests/api/my_spec.rb
+++ spec/requests/api/my_spec.rb
@@ -1,4 +1,17 @@
 require 'swagger_helper'
 
-RSpec.describe 'api/my', type: :request do
+describe 'Api::V1::Cars', type: :request do
+  path '/api/v1/cars' do
+    get 'Returns all cars' do
+      tags 'Cars'
+      produces 'application/json'
+      response '200', 'Returns all cars' do
+        run_test = -> do
+          get '/api/v1/cars'
+          expect(response).to have_http_status(200)
+        end
+        expect(run_test).to change { Car.count }.by(1)
+      end
+    end
+  end
 end
