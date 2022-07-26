596f7693cb40b8bd098265ebb0b02c4e3a81dfef
Nicholas Mutale <nicomutale@gmail.com>
2022-07-26 20:18:49 +0200

    Update car spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   has_many :cars, dependent: :destroy
   has_many :reservations, dependent: :destroy
 
-#   validates :username, presence: true, length: { minimum: 2, maximum: 50 }
-#   validates :password, presence: true, length: { minimum: 6, maximum: 50 }
+  validates :username, presence: true, length: { minimum: 2, maximum: 50 }
+  validates :password, presence: true, length: { minimum: 6, maximum: 50 }
 end
