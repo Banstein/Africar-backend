7456606caf1b0bbaeceed829330d76ecc3e33866
Nicholas Mutale <nicomutale@gmail.com>
2022-07-28 11:55:16 +0200

    Add sessions controller methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,25 @@
 class SessionsController < ApplicationController
+    def create
+        user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+            token = encode_token({ user_id: user.id })
+            render json: { user: user, token: token }, status: :ok
+        else
+            render json: { error: 'Invalid username or password' }, status: :unprocessable_entity
+        end
+    end
+
+    def logged_in
+        if logged_in?
+            render json: { message: 'logged in' }, status: :ok
+        else
+            render json: { error: 'Not logged in' }, status: :unauthorized
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
 end
