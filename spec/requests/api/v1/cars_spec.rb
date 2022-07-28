aa758532c7bb4dafb8461280cfe9b84e70aa6037
Nicholas Mutale <nicomutale@gmail.com>
2022-07-28 06:14:55 +0200

    Create an integration spec to describe and test car API

--- spec/requests/api/v1/cars_spec.rb
+++ spec/requests/api/v1/cars_spec.rb
@@ -1,9 +1,34 @@
 require 'rails_helper'
+RSpec.describe Car, type: :model do
+  subject do
+    @user = User.create(username: 'test', password_digest: 'test', created_at: Time.now, updated_at: Time.now)
+    @car = Car.create(name: 'test', description: 'test', picture: 'car', price: 200, created_at: Time.now,
+                      updated_at: Time.now)
+  end
+  before { subject.save }
 
-RSpec.describe 'Api::V1::Cars', type: :request do
-  describe 'GET /index' do
-    
+  it 'is not valid without a name' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
 
-    pending "add some examples (or delete) #{__FILE__}"
+  it 'is not valid without a description' do
+    subject.description = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without a picture' do
+    subject.picture = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without a price' do
+    subject.price = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without a user_id' do
+    subject.user_id = nil
+    expect(subject).to_not be_valid
   end
 end
