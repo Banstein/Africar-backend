2c7bb833aba3615b166fd9943de186dffccb1c31
Islam Bahnas <islambahnas1@gmail.com>
2022-07-29 16:06:36 +0200

    Add Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+users = User.create!([
+  {
+    username: "bahnas",
+    password_digest: "111111",
+  },
+  {
+    username: "pascal",
+    password_digest: "222222",
+  },
+  {
+    username: "nicholas",
+    password_digest: "333333",
+  },
+])
+
+id: integer, name: string, description: text, picture: string, price: decimal, created_at: datetime, updated_at: datetime, user_id: integer
+Cars = Car.create!([
+  {
+    user_id: User.first
+    name: "toyota",
+    description: "best of the bests"
+    picture: "https://toyota.com.eg/storage/17011/CORO1811-1001.jpg",
+    price: 100.00
+    created_at: Time.now,
+    updated_at: Time.now,
+  },
+  {
+    user_id: User.first
+    name: "honda",
+    description: "best of the lux"
+    picture: "https://tdrresearch.azureedge.net/photos/chrome/Expanded/White/2022HOC020004/2022HOC02000401.jpg",
+    price: 150.00
+    created_at: Time.now,
+    updated_at: Time.now,
+  },
+  {
+    user_id: User.first
+    name: "BMW",
+    description: "best of sedan",
+    picture: "http://clipart-library.com/img/1883029.png",
+    price: 300.00,
+    created_at: Time.now,
+    updated_at: Time.now,
+  },
+])
