7b02c03b2a641545d23d8e984617d787a5d9982e
Islam Bahnas <islambahnas1@gmail.com>
2022-08-04 03:00:05 +0200

    Update model & db, remove users from cars
    fix relations

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,10 +1,10 @@
 class Car < ApplicationRecord
-  belongs_to :user
-  has_many :reservation, dependent: :destroy
+  has_many :reservations, dependent: :destroy
+  has_many :users, through: :reservations, dependent: :destroy
 
   validates :name, presence: true, length: { minimum: 2, maximum: 50 }
   validates :description, presence: true, length: { minimum: 3, maximum: 500 }
-  validates :picture, presence: true
+  validates :picture, presence: true, numericality: { greater_than: 0 }
   validates :price, presence: true
   validates :user_id, presence: true
 end
